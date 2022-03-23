09f122b9b2c88621abc8ef31bbcf7233f4406095
Rohan Kumar <rohank92@users.noreply.github.com>
2022-03-23 12:46:46 -0400

    Update EasyDifficultyRandomizedOrder.sql
    Added 6 CITY table questions

--- SQL/EasyDifficultyRandomizedOrder.sql
+++ SQL/EasyDifficultyRandomizedOrder.sql
@@ -26,3 +26,27 @@ ORDER BY name
 SELECT name FROM Employee 
 WHERE salary > 2000 AND months < 10
 ORDER BY employee_id asc
+
+-- Query a count of the number of cities in CITY having a Population larger than 100,000.
+SELECT COUNT(NAME) FROM CITY
+WHERE POPULATION > 100000
+
+--Query the total population of all cities in CITY where District is California. 
+SELECT SUM(POPULATION) FROM CITY
+WHERE DISTRICT = 'California'
+
+--Query the average population of all cities in CITY where District is California. 
+SELECT AVG(POPULATION) FROM CITY
+WHERE DISTRICT = 'California'
+
+--Query the average population for all cities in CITY, rounded DOWN to the nearest integer.
+-- (round up and round down) 
+SELECT ROUND(AVG(POPULATION)) FROM CITY
+
+--Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN.
+SELECT SUM(POPULATION) FROM CITY 
+WHERE COUNTRYCODE = 'JPN'
+
+--Query the difference between the maximum and minimum populations in CITY.
+SELECT MAX(POPULATION) - MIN(POPULATION) FROM CITY
+
